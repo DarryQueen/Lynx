04b6be6a116f6738f26b00f913bbbbf378b706e4
DarryQueen <slimfrog@gmail.com>
2015-02-21 11:30:46 -0800

    Added orientation detection for handshake.

--- app/src/main/java/com/catlynx/dzd/lynx/handshakerdetector/HandShakeDetector.java
+++ app/src/main/java/com/catlynx/dzd/lynx/handshakerdetector/HandShakeDetector.java
@@ -9,23 +9,29 @@ import android.os.SystemClock;
 import android.util.Log;
 
 public class HandShakeDetector {
-    private SensorManager mgr = null;
+    private static final double THRESHOLD_ANGLE = Math.PI / 4;
+
+    private SensorManager mgr;
     private long lastShakeTimestamp = 0;
+    private boolean horizontal = false;
     private double highThresh;
-    private double lowThresh;
     private long gapTime;
     private HandShakeDetector.Callback cb = null;
 
     public HandShakeDetector(Context ctxt, double highThresh,
                              long gapTime, HandShakeDetector.Callback cb) {
         this.highThresh = highThresh;
-        this.lowThresh = lowThresh;
         this.gapTime = gapTime;
         this.cb = cb;
 
         mgr = (SensorManager) ctxt.getSystemService(Context.SENSOR_SERVICE);
+        // Acceleration listener:
         mgr.registerListener(listener,
                 mgr.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),
+                SensorManager.SENSOR_DELAY_UI);
+        // Orientation listener:
+        mgr.registerListener(listener,
+                mgr.getDefaultSensor(Sensor.TYPE_GRAVITY),
                 SensorManager.SENSOR_DELAY_UI);
     }
 
@@ -37,7 +43,7 @@ public class HandShakeDetector {
         long now = SystemClock.uptimeMillis();
         lastShakeTimestamp = now;
 
-        if (cb != null) cb.shakingStarted();
+        if (cb != null && horizontal) cb.shakingStarted();
     }
 
     private void isNotShaking() {
@@ -52,6 +58,12 @@ public class HandShakeDetector {
         }
     }
 
+    private boolean isHorizontal(double xGrav, double yGrav, double zGrav) {
+        double xzGrav = Math.sqrt(Math.pow(xGrav, 2)
+                + Math.pow(zGrav, 2));
+        return Math.abs(yGrav / xzGrav) >= Math.tan(THRESHOLD_ANGLE);
+    }
+
     public static interface Callback {
         void shakingStarted();
         void shakingStopped();
@@ -59,19 +71,19 @@ public class HandShakeDetector {
 
     private SensorEventListener listener = new SensorEventListener() {
         public void onSensorChanged(SensorEvent e) {
-            if (e.sensor.getType() != Sensor.TYPE_LINEAR_ACCELERATION) {
-                return;
-            }
+            if (e.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+                // Absolute acceleration:
+                double acceleration = Math.sqrt(Math.pow(e.values[0], 2)
+                        + Math.pow(e.values[1], 2)
+                        + Math.pow(e.values[2], 2));
 
-            // Absolute acceleration:
-            double acceleration = Math.sqrt(Math.pow(e.values[0], 2)
-                    + Math.pow(e.values[1], 2)
-                    + Math.pow(e.values[2], 2));
-
-            if (acceleration > highThresh) {
-                isShaking();
-            } else {
-                isNotShaking();
+                if (acceleration > highThresh) {
+                    isShaking();
+                } else {
+                    isNotShaking();
+                }
+            } else if (e.sensor.getType() == Sensor.TYPE_GRAVITY) {
+                horizontal = isHorizontal(e.values[0], e.values[1], e.values[2]);
             }
         }
 
